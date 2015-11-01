998f7f92dd06c2d401ffeeed299327242fb8f83e
Gilberto Villa <ingilniero@gmail.com>
2015-11-01 07:52:47 -0600

    Validate authorsList props

--- src/components/authors/authorsList.js
+++ src/components/authors/authorsList.js
@@ -3,15 +3,20 @@
 var React = require('react');
 
 var AuthorsList = React.createClass({
-  createAuthorRow: function(author) {
-    return (
-      <tr key={author.id}>
-        <td><a href={'/#authors/' + author.id}>{author.id}</a></td>
-        <td>{author.firstName} {author.lastName}</td>
-      </tr>
-    );
+  propTypes: {
+    authors: React.PropTypes.array.isRequired
   },
   render: function() {
+
+    var createAuthorRow = function(author) {
+      return (
+        <tr key={author.id}>
+          <td><a href={'/#authors/' + author.id}>{author.id}</a></td>
+          <td>{author.firstName} {author.lastName}</td>
+        </tr>
+      );
+    };
+
     return (
       <table className='table'>
         <thead>
@@ -19,7 +24,7 @@ var AuthorsList = React.createClass({
           <th>Name</th>
         </thead>
         <tbody>
-          {this.props.authors.map(this.createAuthorRow, this)}
+          {this.props.authors.map(createAuthorRow, this)}
         </tbody>
       </table>
     );
