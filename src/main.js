a63713276a2e8fb50204230888bb21073aac3c6c
Gilberto Villa <ingilniero@gmail.com>
2015-11-01 00:18:54 -0600

    Use iffe to get rid of eslint warning on main.js file

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
-$ = jQuery = require('jquery');
+"use strict";
+
+(function(window) {
+  window.$ = window.jQuery = require('jquery');
+})(window);
+
 var React = require('react');
 var HomePage = require('./components/homePage');
 
