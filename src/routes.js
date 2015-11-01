48828c7d4a0ed079fae485cca9c2d2bdda719523
Gilberto Villa <ingilniero@gmail.com>
2015-11-01 08:33:47 -0600

    Add a notFound page

--- src/routes.js
+++ src/routes.js
@@ -3,18 +3,21 @@
 var React = require('react');
 var Router = require('react-router');
 var DefaultRoute = Router.DefaultRoute;
+var NotFoundRoute = Router.NotFoundRoute;
 var Route = Router.Route;
 
 var App = require('./components/app');
 var HomePage = require('./components/homePage');
 var AuthorsPage = require('./components/authors/authorsPage');
 var AboutPage = require('./components/about/aboutPage');
+var NotFound = require('./components/notFound');
 
 var routes = (
   <Route name='app' path='/' handler={App}>
     <DefaultRoute handler={HomePage} />
     <Route name='authors' handler={AuthorsPage} />
     <Route name='about' handler={AboutPage} />
+    <NotFoundRoute handler={NotFound} />
   </Route>
 );
 
