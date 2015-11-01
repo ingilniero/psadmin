3594d4b37cde563f3e3197bc20f8286e42218173
Gilberto Villa <ingilniero@gmail.com>
2015-11-01 14:58:38 -0600

    Add change listeners to authorsPage component

--- src/components/authors/authorsPage.js
+++ src/components/authors/authorsPage.js
@@ -5,12 +5,23 @@ var Link = require('react-router').Link;
 var AuthorStore = require('../../stores/authorStore');
 var AuthorActions = require('../../actions/authorActions');
 var AuthorsList = require('./authorsList');
+var AuthorStore = require('../../stores/authorStore');
+var AuthorActions = require('../../constants/authorActions');
 
 var AuthorsPage = React.createClass({
   getInitialState: function() {
     return {
       authors: AuthorStore.getAllAuthors()
     };
+  },
+  componentDidMount: function() {
+    AuthorStore.addChangeListener(this._onChange);
+  },
+  componendWillUnmount: function() {
+    AuthorStore.removeChangeListener(this._onChange);
+  },
+  _onChange: function() {
+    this.setState({ authors: AuthorStore.getAllAuthors() });
   },
   render: function() {
     return (
