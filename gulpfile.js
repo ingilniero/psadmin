ed532c2b4b44d02f6fad0f95c8b9275f4ddf0812
Gilberto Villa <ingilniero@gmail.com>
2015-10-31 21:31:09 -0600

    Add wacht gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -36,5 +36,9 @@ gulp.task('html', function() {
     .pipe(connect.reload());
 });
 
-gulp.task('default', ['html', 'open']);
+gulp.task('watch', function() {
+  gulp.watch(config.paths.html, ['html']);
+});
+
+gulp.task('default', ['html', 'open', 'watch']);
 
