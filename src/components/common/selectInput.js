bc507936e547c29aef2ce866f5274269d3b23053
Gilberto Villa <ingilniero@gmail.com>
2015-11-01 22:11:51 -0600

    Add selectInput component

---
+++ src/components/common/selectInput.js
@@ -0,0 +1,33 @@
+"use strict";
+
+var React = require('react');
+
+var SelectInput = React.createClass({
+  propTypes: {
+    label: React.PropTypes.string.isRequired,
+    name: React.PropTypes.string.isRequired,
+    options: React.PropTypes.array.isRequired,
+    value: React.PropTypes.string,
+    onChange: React.PropTypes.func.isRequired
+  },
+  render: function() {
+    var createOption = function(option) {
+        return (
+          <option key={option.name}>{option.name}</option>
+        );
+    };
+
+    return (
+      <div className='form-group'>
+        <label htmlFor={this.props.name}>{this.props.label}</label>
+        <div className='field'>
+          <select onChange={this.props.onChange} name={this.props.name} className='form-control' value={this.props.value}>
+            {this.props.options.map(createOption, this)}
+          </select>
+        </div>
+      </div>
+    );
+  }
+});
+
+module.exports = SelectInput;
